9c7848913b8d0095cf39c61e68c62f28c6de1799
ka-ba <ka-ba@github>
2017-09-18 01:08:45 +0200

    bit more preferences cleanup

--- app/src/main/java/kaba/yucata/envoy/SettingsFragment.java
+++ app/src/main/java/kaba/yucata/envoy/SettingsFragment.java
@@ -14,50 +14,51 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Prefer
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_gamecount);
+        final EditTextPreference p_username = (EditTextPreference) findPreference(getString(R.string.k_pref_username));
+        final EditTextPreference p_password = (EditTextPreference) findPreference(getString(R.string.k_pref_password));
+        final ListPreference p_interval = (ListPreference) findPreference(getString(R.string.k_pref_interval_min));
+        // p_notification not needed here
         // pre-set summaries
-        setSummary( findPreference(getString(R.string.k_pref_username)), null);
-        setSummary( findPreference(getString(R.string.k_pref_password)), null);
-        setSummary( findPreference(getString(R.string.k_pref_interval_min)), null);
+        setSummary(p_username,p_username.getText());
+        setSummary(p_password,p_password.getText());
+        setSummary(p_interval,p_interval.getValue());
+        setNotiActivityAfterInterval(p_interval.getValue());
         // register listeners for updating summaries
-        findPreference(getString(R.string.k_pref_username)).setOnPreferenceChangeListener(this);
-        findPreference(getString(R.string.k_pref_password)).setOnPreferenceChangeListener(this);
-        findPreference(getString(R.string.k_pref_interval_min)).setOnPreferenceChangeListener(this);
+        p_username.setOnPreferenceChangeListener(this);
+        p_password.setOnPreferenceChangeListener(this);
+        p_interval.setOnPreferenceChangeListener(this);
     }
 
-    private void setSummary(Preference pref, Object new_val) {
-        // if newVal is pecified, use it; else obtain below
-        String summary=( new_val==null ? null : new_val.toString() );
+//    private void setSummary(Preference pref) {
+//        if( pref instanceof EditTextPreference )
+//            setSummary( pref, ((EditTextPreference) pref).getText() );
+//        else if( pref instanceof ListPreference )
+//            setSummary( pref, ((ListPreference) pref).getEntry() );
+//        else
+//            throw new InternalError("implement additional preferences type here");
+//    }
+
+    private void setSummary(Preference pref, Object val) {
+        String summary=null;
 
         // username
         if(pref.getKey().equals( getString(R.string.k_pref_username) )) {
-            if(summary==null)
-                summary = ((EditTextPreference) pref).getText();
-            if( (summary==null) || (summary.length()<1) )
-                summary=getText(R.string.username_init_txt).toString();
+            final String sval = (String) val;
+            summary = ( (sval==null)||(sval.length()<1)
+                    ? getText(R.string.username_init_txt).toString() : sval );
         // password / secret
         } else if(pref.getKey().equals( getString(R.string.k_pref_password) )) {
-            if (new_val != null) {
-                if (((String) new_val).length() == 0)
-                    summary = "Pasword not specified";
-                else
-                    summary = "Password set";  // TODO: mark if successfully used or rejected
-            } else {
-                final String pw = ((EditTextPreference) pref).getText();
-                if ((pw == null) || (pw.length() == 0))
-                    summary = "Pasword not specified";
-                else
-                    summary = "Password set";  // TODO: mark if successfully used or rejected
-            }
+            final String sval = (String) val;
+            summary = ( (sval==null)||(sval.length()<1)
+                    ? "Pasword not specified" : "Password set" );
         // polling interval
         } else if(pref.getKey().equals( getString(R.string.k_pref_interval_min) )) {
+            final String sval = (String) val;
             final ListPreference lpref = (ListPreference) pref;
-            if (summary == null)
-                summary = ""+lpref.getEntry();
-            else
-                summary = lpref.getEntries()[lpref.findIndexOfValue((String)new_val)].toString();
-            summary = "Interval set to "+summary;
+            final CharSequence text = lpref.getEntries()[lpref.findIndexOfValue(sval)];
+            summary = ( (sval==null)||(sval.equals(getText(R.string.polling_intervals_minutes_never).toString()) )
+                    ? "no automatic updates" : "Interval set to "+text );
         }
-
         if(summary!=null) {
             pref.setSummary(summary);
             System.out.println("setting "+pref.getKey()+"'s summary to "+summary);
@@ -67,6 +68,17 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Prefer
     @Override
     public boolean onPreferenceChange(Preference pref, Object newValue) {
         setSummary(pref, newValue);
+        if(pref.getKey().equals( getString(R.string.k_pref_interval_min) ))
+            setNotiActivityAfterInterval(newValue);
         return true;
+    }
+
+    private void setNotiActivityAfterInterval(Object newValue) {
+        final String cval = (String) newValue;
+        final Preference noti_pref = findPreference(getString(R.string.k_pref_notifications));
+        if( cval.equals(getText(R.string.polling_intervals_minutes_never).toString()) )
+            noti_pref.setEnabled(false);
+        else
+            noti_pref.setEnabled(true);
     }
 }
