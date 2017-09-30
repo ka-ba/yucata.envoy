518ce0ccfc70b008e6da88fbfab82dcc6ad1a478
ka-ba <ka-ba@github>
2017-09-30 17:36:36 +0200

    about

---
+++ app/src/main/java/kaba/yucata/envoy/AboutActivity.java
@@ -0,0 +1,21 @@
+package kaba.yucata.envoy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.TextView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        TextView tv = (TextView) findViewById(R.id.about_lic_tv);
+        tv.setMovementMethod(LinkMovementMethod.getInstance());
+        tv.setText(Html.fromHtml(getString(R.string.about_lic_text)));
+    }
+}
--- app/src/main/java/kaba/yucata/envoy/SettingsActivity.java
+++ app/src/main/java/kaba/yucata/envoy/SettingsActivity.java
@@ -1,7 +1,13 @@
 package kaba.yucata.envoy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import kaba.yucata.envoy.util.DiagnosticActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +15,27 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        final MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mitem_about:
+                final Intent about_intent = new Intent(this, AboutActivity.class);
+                startActivity(about_intent);
+                return true;
+            case R.id.mitem_diagnostic:
+                final Intent diagnostic_intent = new Intent(this, DiagnosticActivity.class);
+                startActivity(diagnostic_intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
